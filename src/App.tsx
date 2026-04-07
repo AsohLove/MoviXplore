10163002abef870bd83b4d1617d537274828173a
Love Asoh <loveasoh@gmail.com>
2026-04-07 11:34:02 +0100

    remove .env from tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
-import Favorites from "./components/Favorites";
+import Favorites from "./pages/Favorites";
 import DashboardLayout from "./layout/DashboardLayout";
 
 export default function App() {
   return (
-      <Routes>
-        <Route element={<DashboardLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="favorite" element={<Favorites />} />
-        </Route>
-        
-      </Routes>
-
+    <Routes>
+      <Route element={<DashboardLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="favorite" element={<Favorites />} />
+      </Route>
+    </Routes>
   );
 }
